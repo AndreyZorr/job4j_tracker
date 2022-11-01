cca8cbc63c8f4783bd0557afd80a81b0bbd5865c
Andrey <AlpatoV_A_A@mail.ru>
2022-11-01 15:13:28 +0300

    1.1. Создание объекта [#165452 #356410]

--- src/main/java/ru/job4j/tracker/oop/Cat.java
+++ src/main/java/ru/job4j/tracker/oop/Cat.java
@@ -1,8 +1,25 @@
 package ru.job4j.tracker.oop;
 
 public class Cat {
-        public static void main(String[] args) {
-            Cat peppy = new Cat();
-            Cat sparky = new Cat();
-        }
+
+    private String food;
+
+    public void show() {
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.show();
+    }
 }
