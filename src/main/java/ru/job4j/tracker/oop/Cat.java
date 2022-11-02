695b995d3546be359a52753f0e1c020d5ac7b669
Andrey <AlpatoV_A_A@mail.ru>
2022-11-02 22:21:01 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/tracker/oop/Cat.java
+++ src/main/java/ru/job4j/tracker/oop/Cat.java
@@ -2,10 +2,15 @@ package ru.job4j.tracker.oop;
 
 public class Cat {
 
+    private String food;
     private String name;
 
     public void show() {
-        System.out.println(this.name);
+        System.out.println(this.food + this.name);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
     }
 
     public void giveNick(String nick) {
@@ -13,11 +18,15 @@ public class Cat {
     }
 
     public static void main(String[] args) {
-        System.out.println("Garage.");
-        Cat garage = new Cat();
-        garage.giveNick("Garage, ate food.");
-        garage.show();
-
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Puh");
+        black.eat("fish");
+        black.show();
     }
-
 }
