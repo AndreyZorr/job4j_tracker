d84159a246bf93f1d6b63346c6f272fcc2eba43b
Andrey <AlpatoV_A_A@mail.ru>
2022-11-03 23:13:22 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/tracker/oop/Ball.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker.oop;
+
+public class Ball {
+
+    public void tryRun(boolean condition) {
+        if (condition) {
+            System.out.println("Bun eaten.");
+        } else {
+            System.out.println("Bun ran away.");
+        }
+    }
+}
