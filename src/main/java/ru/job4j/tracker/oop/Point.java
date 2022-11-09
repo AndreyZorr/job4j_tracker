a638556744361462b05fbd0d7ddd002ed63416db
Andrey <AlpatoV_A_A@mail.ru>
2022-11-09 10:09:49 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587 #358868]

--- src/main/java/ru/job4j/tracker/oop/Point.java
+++ src/main/java/ru/job4j/tracker/oop/Point.java
@@ -6,7 +6,6 @@ import static java.lang.Math.pow;
 public class Point {
 
     private int x;
-
     private int y;
 
     public Point(int first, int second) {
@@ -15,7 +14,7 @@ public class Point {
     }
 
     public double distance(Point that) {
-        return sqrt(pow(this.x - that.x, 2) + pow(this.y + that.y, 2));
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
--- src/test/java/ru/job4j/tracker/oop/PointTest.java
+++ src/test/java/ru/job4j/tracker/oop/PointTest.java
@@ -6,27 +6,27 @@ import org.junit.jupiter.api.Test;
 public class PointTest {
 
     @Test
-    public void when00to20then2() {
-        double expected = 2;
-        Point a = new Point(0, 0);
-        Point b = new Point(2, 0);
+    public void when11to11then0() {
+        double expected = 0;
+        Point a = new Point(1, 1);
+        Point b = new Point(1, 1);
         double dist = a.distance(b);
         Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
-    public void when14to28then12dot04() {
-        double expected = 12.04;
-        Point a = new Point(1, 4);
-        Point b = new Point(2, 8);
+    public void when56to78then2dot82() {
+        double expected = 2.82;
+        Point a = new Point(5, 6);
+        Point b = new Point(7, 8);
         double dist = a.distance(b);
         Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
-    public void when35to68then13dot34() {
-        double expected = 13.34;
-        Point a = new Point(3, 5);
+    public void when59to68then1dot41() {
+        double expected = 1.41;
+        Point a = new Point(5, 9);
         Point b = new Point(6, 8);
         double dist = a.distance(b);
         Assert.assertEquals(expected, dist, 0.01);
