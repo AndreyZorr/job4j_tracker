c1fd96a6efbab49b2708efd48c45ca04a50af649
Andrey <AlpatoV_A_A@mail.ru>
2023-02-08 15:00:55 +0300

    1. Интерфейс Collection [#504820 #389631]

---
+++ src/main/java/ru/job4j/collection/CollectionAddCollection.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CollectionAddCollection {
+    public static void main(String[] args) {
+        Collection<String> collection = new ArrayList<>();
+        collection.add("one");
+        collection.add("two");
+        collection.add("three");
+        Collection<String> list = new ArrayList<>(); //добавил коллекцию в другую коллекцию
+        list.addAll(collection);
+        for (String str : list) {
+            System.out.println(str);
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/collection/CollectionRetainAll.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CollectionRetainAll {
+    public static void main(String[] args) {
+        Collection<String> collection = new ArrayList<>();
+        collection.add("one");
+        collection.add("two");
+        collection.add("three");
+        Collection<String> list = new ArrayList<>();
+        list.add("two");
+        for (String str : collection) {
+            System.out.println(str);
+        }
+        collection.retainAll(list);
+        System.out.println("Вывод содержимого коллекции после вызова retainAll");
+        for (String str : collection) {
+            System.out.println(str);
+        }
+    }
+}
