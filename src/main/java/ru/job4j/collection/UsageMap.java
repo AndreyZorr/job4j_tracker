0005c8128a3c445f0caf99728d18402e139a7dcf
Andrey <AlpatoV_A_A@mail.ru>
2023-02-27 22:10:28 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("alpatoV_a_a@mail.ru", "Alpatov Andrey Anatolvech");
+        map.put("peran@@yandex.ru", "Semin Pavel Ivanovich");
+        map.put("sergo@gmail.com", "Kuzmin Sergei Pavlovich");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + "=" + value);
+        }
+    }
+}
