ede2fa8913d8ca46badf6a66db419478f638ec26
Andrey <AlpatoV_A_A@mail.ru>
2023-03-21 23:26:17 +0300

    2. Что такое JavaDoc

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -2,6 +2,12 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс модели банковского счета
+ * @author Andrey Alpatov
+ * @version 1.0
+ */
+
 public class Account {
     private String requisite;
     private double balance;
