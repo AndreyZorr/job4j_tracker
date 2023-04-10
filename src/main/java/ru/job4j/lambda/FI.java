f656f17ea370d417a76e7f9794dabc833de30400
Andrey <AlpatoV_A_A@mail.ru>
2023-04-10 11:18:59 +0300

    2.0. Лямбда  [#58165]

---
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -0,0 +1,26 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class FI {
+    public static void main(String[] args) {
+        String[] st = {
+                "image 11",
+                "image 3",
+                "image 2",
+        };
+        Comparator<String> cmpText = (left, right) -> left.compareTo(right);
+        Arrays.sort(st, cmpText);
+        for (String string : st) {
+            System.out.println(string);
+        }
+        System.out.println();
+        Comparator<String> cmpDescSize = (left, right) -> Integer.compare(left.length(), right.length());
+                Arrays.sort(st, cmpDescSize);
+        for (String string : st) {
+            System.out.println(string);
+        }
+    }
+
+}
