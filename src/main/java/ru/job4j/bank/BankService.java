1320a209d71dc2db40435f8262d61e27ae5bcc5c
Andrey <AlpatoV_A_A@mail.ru>
2023-04-26 10:19:20 +0300

    17. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -1,114 +1,110 @@
 package ru.job4j.bank;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 /**
- * Класс реализует Банковскую систему
- * @author  Andrey Alpatov
- * @version 1.4
+ * Класс описывает работу модели банковской системы
+ * @author Andrey Alpatov
+ * @version 1.0
  */
 public class BankService {
+    /**
+     * Поле содержит всех пользователей системы с привязанными к ним счетами
+     */
     private final Map<User, List<Account>> users = new HashMap<>();
     /**
-     * Метод добавляет нового пользователя в систему,
-     * в случае если его еще нет в системе
-     * @param user пользователь которого нужно добавить в систему
+     * Метод добавляет пользователя в систему
+     * @param user класса User будет добавлен в Map users
+     * При добавлении происходит проверка, что пользователя ещё нет в системе.
      */
 
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
     /**
-     * Метод удалаяет пользователя из системы, по данным паспорта
-     * @param passport паспорт удаляемого пользователя
-     * @return возвращает {true} если перед удалением удалось найти пользователя по заданным параметрам,
-     * иначе если такого пользователя не было в системе возвращает {false}
+     * Данный метод позволяет удалить пользователя из системы
+     * @param passport является уникальным идентификатором, на основании которого происходит удачение
+     * @return true, если пользователь был найден и удалён. Иначе вернёт false
      */
 
     public boolean deleteUser(String passport) {
         return users.remove(new User(passport, "")) != null;
     }
     /**
-     * Метод добавляет новый аккаунт к списку аккаунтов пользователя,
-     * в случае если удалось найти пользователя по паспорту
-     * @param passport паспорт пользователя
-     * @param account аккаунт который нужно добавить в список аккаунтов пользователя
+     * Данный метод добавляет новый счет к пользователю
+     * @param passport является уникальным идентификатором, на основании которого происходит
+     * поиск пользователя с помощью метода findByPassport(passport), после чего происходит
+     * поиск списка счетов данного пользователя
+     * @param account используется для проверки, что такого счета у пользователя еще нет.
      */
 
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
-            List<Account> userAccounts = users.get(user);
-            if (!userAccounts.contains(account)) {
-                userAccounts.add(account);
+            List<Account> accountList = getAccounts(user);
+            if (!accountList.contains(account)) {
+                accountList.add(account);
             }
         }
     }
     /**
-     * Метод позволяет найти пользователя в системе по паспорту
-     * @param passport паспорт пользователя
-     * @return Возврящает пользователя в формате User {@link User} если удалось его найти,
-     * иначе возвращяет null
+     * Этот метод ищет пользователя по номеру паспорта.
+     * @param passport используется для поиска пользователя
+     * @return объект найденного пользователя класса User
      */
 
     public User findByPassport(String passport) {
         return users.keySet()
                 .stream()
-                .filter(user -> user.getPassport().equals(passport))
-                .findFirst().orElse(null);
+                .filter(p -> p.getPassport().equals(passport))
+                .findFirst()
+                .orElse(null);
     }
 
     /**
-     * Метод позволяет найти аккаунт пользователя по реквизитам,
-     * перед этим найдя пользователя по паспорту
-     * @param passport паспорт пользователя
-     * @param requisite реквизиты аккаунта
-     * @return Возврящает аккаунт пользователя в формате Account {@link Account} если удалось его найти,
-     * иначе возвращяет {null}
+     * Метод ищет счет пользователя по реквизитам:
+     * @param passport используется для поиска пользователя по пасспорту
+     * @param requisite используется для поиска счёта у пользователя
+     * @return объёкт найденного счёта класса Account
      */
-
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
-            return users.get(user)
-                    .stream()
-                    .filter(account -> account.getRequisite().equals(requisite))
-                    .findFirst().orElse(null);
+             return getAccounts(user).
+                    stream()
+                    .filter(p -> p.getRequisite().equals(requisite))
+                    .findFirst()
+                    .orElse(null);
         }
         return null;
     }
-
     /**
-     * Метод позволяет перевести деньги с одного аккаунта на другой,
-     * Если находит пользователей по паспорту и аккаунт по реквизитам
-     * @param srcPassport паспорт пользователя с аккаунта которого происходит списание средств
-     * @param srcRequisite реквизиты аккаунта с которого происходит списание средств
-     * @param destPassport паспорт пользователя на аккаунт которого происходит перевод средств
-     * @param destRequisite реквизиты аакаунта на который происходит перевод средств
-     * @param amount сумма перевода
-     * @return возвращает {true} если аакаунт отправитель и аакаунт получатель найдены, и баланс ааканта отправителя
-     * больше и равен сумме перевода.
-     * иначе возврящает {false}
+     * Метод предназначен для перечисления денег с одного счёта на другой счёт.
+     * @param srcPassport используется для поиска пользователя отправителя
+     * @param srcRequisite используется для поиска счёта пользователя отправителя
+     * @param destPassport используется для поиска пользователя получателя
+     * @param destRequisite используется для поиска счёта пользователя получателя
+     * @param amount количество перечисляемых средств
+     * @return true при успешном перечислении средств, иначе возвращает false
      */
+
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
-        Account srsAccount = findByRequisite(srcPassport, srcRequisite);
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
         Account destAccount = findByRequisite(destPassport, destRequisite);
-        if (srsAccount != null && destAccount != null && srsAccount.getBalance() >= amount) {
-            srsAccount.setBalance(srsAccount.getBalance() - amount);
+        if (srcAccount != null && srcAccount.getBalance() >= amount && destAccount != null) {
+            srcAccount.setBalance(srcAccount.getBalance() - amount);
             destAccount.setBalance(destAccount.getBalance() + amount);
             rsl = true;
         }
         return rsl;
     }
     /**
-     * Метод позволяет найти список всех аккаунтов пользователя
-     * @param user пользователь в формате User {@link User}
-     * @return Возврящает список всех аккаунтов пользователя,
-     * если пользовательне не найден возвращянт {null}
+     * Метод возвращает список всех счетов пользователя
+     * @param user используется для поиска пользователя
+     * @return список всех счетов пользователя
      */
 
     public List<Account> getAccounts(User user) {
