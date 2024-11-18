e3dccd0370a08516c665457dbbc37601b016bf2e
Andrey <AlpatoV_A_A@mail.ru>
2024-11-18 21:56:48 +0300

    3. @Builder [#504890]

---
+++ src/main/java/ru/job4j/lombok/LombokUsage.java
@@ -0,0 +1,13 @@
+package ru.job4j.lombok;
+
+public class LombokUsage {
+    public static void main(String[] args) {
+        var permission = Permission.of()
+                .id(1)
+                .name("Name")
+                .rules("create")
+                .rules("update")
+                .build();
+        System.out.println(permission);
+    }
+}
